2e693bdd70df002409f946e0036cf6a17adb3fbc
VengeVias <rocktbh@gmail.com>
2020-10-21 21:16:06 +0900

    initial commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: :edit
+
   def index
     @prototypes = Prototype.all
   end
@@ -20,7 +23,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.find(@prototype.id)
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
@@ -50,5 +53,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
 
 end
